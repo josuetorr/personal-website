2b009c5b83be8f254442e05abde1bade0992f88e
josuetorr <josue.dtorres@gmail.com>
2019-08-23 19:26:08 -0400

    get request to /

--- mailer/app.js
+++ mailer/app.js
@@ -8,8 +8,15 @@ require('dotenv').config()
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
 
-// Post request to root directory
-app.post('/', (req, res, next) => {
+// Get request to root directory
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'GET handler'
+    });
+});
+
+// Post request to mailer
+app.post('/sendmail', (req, res, next) => {
     sendMail(req.body.fullName, req.body.email, req.body.subject, req.body.content);
     res.status(200).json({
         message: 'Email sent successfully!'
