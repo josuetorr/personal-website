c5e4fb78eed0d36993d4b724006ff848af40d56b
josuetorr <josue.dtorres@gmail.com>
2019-09-13 15:32:06 -0400

    nav overlay layout done, need to add animation

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,20 +2,24 @@
 	<div id="app">
 		<main>
 			<header id="logo">
-				logo
+				<img src="./assets/img/logo-large.png" alt="logo">
 			</header>
 			<router-view class="page"></router-view>
 		</main>
-		<nav>
-			<div class="nav-links">
-				<router-link class="link" v-for="page in pages" :key="page.url" :to="page.url">{{ page.name }}</router-link>
-			</div>
-			<div class="hamburger">
-				<div class="line"></div>
-				<div class="line"></div>
-				<div class="line"></div>
+		<nav :class="{show: isShowing}">
+			<div @click="toggleVisibility" class="nav-links">
+				<router-link class="link" 
+							
+							v-for="page in pages" 
+							:key="page.url" 
+							:to="page.url">{{ page.name }}</router-link>
 			</div>
 		</nav>
+		<div @click="toggleVisibility" class="hamburger" :class="{active: isActive}">
+				<div class="line"></div>
+				<div class="line"></div>
+				<div class="line"></div>
+		</div>
 	</div>
 </template>
 
@@ -41,87 +45,20 @@
 					url: '/reachme',
 					name: 'REACH ME',
 				}
-			}
+			},
+			isShowing: false,
+			isActive: false,
 		}
 	},
+	methods: {
+		toggleVisibility: function() {
+			this.isShowing = !this.isShowing;	
+			this.isActive = !this.isActive;
+		},
+	}
   }
 </script>
 
 <style lang="scss">
-@import "./scss/_variables.scss";
-@import "./scss/_config.scss";
-
-*, 
-*::before, 
-*::after {
-	margin: 0;
-	padding: 0;
-	box-sizing: box-border;
-}
-
-#app {
-	font-family: 'Anonymous Pro', monospace, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
-	text-align: center;
-	background-color: $bgColor;
-	color: $textColor;
-	height: 100vh;
-	width: 100%;
-	display: grid;
-	grid-template-columns: 3fr 1fr;
-}
-
-#logo {
-	font-size: 6rem;
-	margin: 30px;
-}
-
-main {
-	display: grid;
-	grid-template-rows: 1fr 4fr;
-	padding: 0 6rem;
-}
-
-a {
-	text-decoration: none;
-	color: $textColor;
-
-	&:hover {
-		color: red;	
-	}
-
-}
-
-nav {
-	.nav-links {
-		display: grid;
-		grid-template-rows: repeat(4, 1fr);
-	}
-
-	.hamburger {
-		position: fixed;
-		display: grid;
-		grid-template-rows: repeat(3, 1fr);
-		grid-row-gap: 7px;
-		width: 100%;
-
-		.line {
-			width: 100%;
-			height: 5px;
-			background-color: $textColor;
-		}
-	}
-
-	.router-link-exact-active {
-		font-weight: bold;
-	}
-}
-
-li {
-	list-style: none;
-}
-
-@import "./scss/_responsive.scss";
+@import "./scss/main.scss";
 </style>
--- client/src/components/Reachme.vue
+++ client/src/components/Reachme.vue
@@ -1,7 +1,27 @@
 <template>
-    <h1>
-        Reach me
-    </h1>
+	<div id="reachme">
+		<form action="" method="post" id="email-form">
+			<section class="fullname">
+				<label for="fullname">Name</label>
+				<input type="text" name="fullname" id="fullname" />
+			</section>
+			<section class="email">
+				<label for="email">Email</label>
+				<input id="email" type="email" name="email" />
+			</section>
+			<section class="subject">
+				<label for="subject">Subject</label>
+				<input type="text" id="" name="subject" />
+			</section>
+			<section class="message">
+				<label for="message">Message</label>
+				<textarea name="message" cols="30" rows="5" placeholder="..."></textarea>
+			</section>
+			<section class="submit">
+				<button type="submit">Send</button>
+			</section>
+		</form>
+	</div>
 </template>
 
 <script>
